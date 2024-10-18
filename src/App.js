e1c969b3190adcc9945bf92a8951a61d7aac6b20
Jessie P. <ritime275@gmail.com>
2024-10-18 02:25:34 -0400

    Added footer and Github Url

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import "./App.css";
 
-function App() {
-  return <div className="App">Weather App</div>;
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by{" "}
+        <a href="https://github.com/Comet429" target="_blank">
+          Jessie Polong
+        </a>{" "}
+        and is{" "}
+        <a href="https://github.com/Comet429/react-panahon-app" target="_blank">
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
-
-export default App;
