c3ea7c9d8e301673eeae2f1b45688f4b0e0fd3df
Jessie P. <ritime275@gmail.com>
2024-10-18 02:32:05 -0400

    Edited footer

--- src/App.js
+++ src/App.js
@@ -6,11 +6,15 @@ export default function App() {
       <h1>Weather App</h1>
       <footer>
         This project was coded by{" "}
-        <a href="https://github.com/Comet429" target="_blank">
+        <a href="https://github.com/Comet429" target="_blank" rel="noreferrer">
           Jessie Polong
         </a>{" "}
         and is{" "}
-        <a href="https://github.com/Comet429/react-panahon-app" target="_blank">
+        <a
+          href="https://github.com/Comet429/react-panahon-app"
+          target="_blank"
+          rel="noreferrer"
+        >
           open-sourced on GitHub
         </a>
       </footer>
